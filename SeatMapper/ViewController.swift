c18879a4ac98f73f0dcd07776b12980783b8f82a
yoga <yogamadness@gmail.com>
2018-11-27 09:47:31 +0700

    Initial Commit

---
+++ SeatMapper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeatMapper
+//
+//  Created by Yoga Pratama on 27/11/18.
+//  Copyright © 2018 YPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
